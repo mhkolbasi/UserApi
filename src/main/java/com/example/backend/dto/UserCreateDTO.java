11813727e6d0b3d69fa0dec5c7e40c33a42c60cf
mhkolbasi <huseyin.kolbasi@hotmail.com>
2021-03-20 20:59:01 +0300

    User Validation

--- src/main/java/com/example/backend/dto/UserCreateDTO.java
+++ src/main/java/com/example/backend/dto/UserCreateDTO.java
@@ -2,13 +2,17 @@ package com.example.backend.dto;
 
 import org.hibernate.annotations.BatchSize;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class UserCreateDTO {
 
-
+    @NotNull(message = "{backend.constraints.firstName.NotNull.message}")
+    @Size(min = 2,max = 30,message = "{backend.constraints.firstName.Size.message}")
     private String firstName;
 
+    @NotNull(message = "{backend.constraints.lastName.NotNull.message}")
+    @Size(min = 2,max = 30,message = "{backend.constraints.lastName.Size.message}")
     private String lastName;
 
     public UserCreateDTO(){
